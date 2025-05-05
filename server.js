1048d586975d629dc5620648a3b5834e01479713
theigniteai <theignite.ai@gmail.com>
2025-05-05 22:00:46 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('AccentShift Backend is live');
+});
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`🟢 Server running on port ${PORT}`);
+});
